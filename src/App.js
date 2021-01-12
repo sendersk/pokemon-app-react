bfc7e3061f52c2aa5d9bc0c8763e077e6b3895ab
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-12 21:44:01 +0100

    navigation section added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 import PokemonList from "./containers/PokemonList";
 import Pokemon from "./containers/Pokemon";
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <NavLink to={"/"}>Search</NavLink>
+      </nav>
       <Switch>
         <Route path="/" exact component={PokemonList} />
         <Route path="/pokemon/:pokemon" exact component={Pokemon} />
