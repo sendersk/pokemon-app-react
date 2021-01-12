9779349c2c06b2f86c8a27838c1d9650650ef3a6
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-12 22:29:51 +0100

    Pokemon content added

--- src/containers/Pokemon.js
+++ src/containers/Pokemon.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { GetPokemon } from "../actions/PokemonActions";
+import _ from "lodash";
 
-function Pokemon() {
-    return (
-        <div>
-            Pokemon
+const Pokemon = (props) => {
+  const pokemonName = props.match.params.pokemon;
+  const dispatch = useDispatch();
+  const pokemonState = useSelector((state) => state.Pokemon);
+
+  useEffect(() => {
+    dispatch(GetPokemon(pokemonName));
+  }, [dispatch, pokemonName]);
+
+  const ShowData = () => {
+    if (!_.isEmpty(pokemonState.data[pokemonName])) {
+      const pokeData = pokemonState.data[pokemonName];
+      return (
+        <div className={"pokemon-wrapper"}>
+          <div className={"item"}>
+            <h1>Sprites</h1>
+            <img src={pokeData.sprites.front_default} alt="" />
+            <img src={pokeData.sprites.back_default} alt="" />
+            <img src={pokeData.sprites.front_shiny} alt="" />
+            <img src={pokeData.sprites.back_shiny} alt="" />
+          </div>
+          <div className="item">
+            <h1>Stats</h1>
+            {pokeData.stats.map(({ stat, base_stat }) => {
+              return (
+                <p key={stat.name}>
+                  {stat.name} {base_stat}
+                </p>
+              );
+            })}
+          </div>
+          <div className="item">
+            <h1>Abilities</h1>
+            {pokeData.abilities.map(({ ability }) => {
+              return <p key={ability.name}>{ability.name}</p>;
+            })}
+          </div>
         </div>
-    )
-}
+      );
+    }
 
-export default Pokemon
+    if (pokemonState.loading) {
+      return <p>Loading...</p>;
+    }
+
+    if (pokemonState.errorMsg !== "") {
+      return <p>{pokemonState.errorMsg}</p>;
+    }
+
+    return <p>error getting pokemon</p>;
+  };
+
+  return (
+    <div className={"poke"}>
+      <h1>{pokemonName}</h1>
+      {ShowData()}
+    </div>
+  );
+};
+
+export default Pokemon;
