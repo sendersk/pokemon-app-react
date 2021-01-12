caeee4a1bab22d6d32af537c8cc1ed37baad9a55
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-12 22:05:25 +0100

    Pokemon list content added

--- src/containers/PokemonList.js
+++ src/containers/PokemonList.js
@@ -1,11 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import _ from "lodash";
+import { GetPokemonList } from "../actions/PokemonActions";
+import { Link } from "react-router-dom";
+import ReactPaginate from "react-paginate";
 
-function PokemonList() {
-    return (
-        <div>
-            Pokemon list
+const PokemonList = (props) => {
+  const [search, setSearch] = useState("");
+  const dispatch = useDispatch();
+  const pokemonList = useSelector((state) => state.PokemonList);
+
+  useEffect(() => {
+    FetchData(1);
+  }, []);
+
+  const FetchData = (page = 1) => {
+    dispatch(GetPokemonList(page));
+  };
+
+  const ShowData = () => {
+    if (pokemonList.loading) {
+      return <p>Loading...</p>;
+    }
+
+    if (!_.isEmpty(pokemonList.data)) {
+      return (
+        <div className={"list-wrapper"}>
+          {pokemonList.data.map((el, id) => {
+            return (
+              <div key={id} className={"pokemon-item"}>
+                <p key={el.name}>{el.name}</p>
+                <Link to={`/pokemon/${el.name}`}>View</Link>
+              </div>
+            );
+          })}
         </div>
-    )
-}
+      );
+    }
 
-export default PokemonList
+    if (pokemonList.errorMsg !== "") {
+      return <p>{pokemonList.errorMsg}</p>;
+    }
+
+    return <p>unable to get data</p>;
+  };
+
+  return (
+    <div>
+      <div className={"search-wrapper"}>
+        <p>Search: </p>
+        <input type="text" onChange={(e) => setSearch(e.target.value)} />
+        <button
+          className={"search-btn"}
+          onClick={() => props.history.push(`/pokemon/${search}`)}
+        >
+          Search
+        </button>
+      </div>
+      {ShowData()}
+      {!_.isEmpty(pokemonList.data) && (
+        <ReactPaginate
+          pageCount={Math.ceil(pokemonList.count / 20)}
+          pageRangeDisplayed={2}
+          marginPagesDisplayed={1}
+          onPageChange={(data) => FetchData(data.selected + 1)}
+          containerClassName={"pagination"}
+        />
+      )}
+    </div>
+  );
+};
+
+export default PokemonList;
