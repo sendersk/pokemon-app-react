827d745bd08607e6488df6dd34c780f0e4040fac
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-14 19:23:55 +0100

    div class name changed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Pokemon from "./containers/Pokemon";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <nav>
         <NavLink to={"/"}>Home</NavLink>
       </nav>
