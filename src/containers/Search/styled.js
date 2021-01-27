2f4fcf86eb3f6d18cdf337fd8dc19ffaa4cdb025
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-27 22:41:37 +0100

    SearchWrapper and SearchParagraph styles added

---
+++ src/containers/Search/styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const SearchWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 50px;
+  justify-content: center;
+  font-size: 20px;
+  align-items: center;
+  margin: 20px 0;
+  padding: 10px 15px;
+  gap: 20px;
+`;
+
+export const SearchParagraph = styled.p`
+  color: #555;
+  font-size: 30px;
+  font-weight: 700;
+`;
