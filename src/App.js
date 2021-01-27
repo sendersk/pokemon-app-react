6eee548478a60df28c8051bf0a5a0a1422999d4c
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-27 22:47:48 +0100

    App.css file deleted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import { Switch, Route, Redirect } from "react-router-dom";
 import PokemonList from "./containers/PokemonList/PokemonList";
 import Pokemon from "./containers/Pokemon/Pokemon";
