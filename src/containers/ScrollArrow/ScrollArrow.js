4da2d8cb4f54dccae5789c126a73bd6f49638528
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-28 22:30:32 +0100

    ScrollArrow component created

--- src/containers/ScrollArrow/ScrollArrow.js
+++ src/containers/ScrollArrow/ScrollArrow.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { ScrollTop } from "./styled";
 
-function ScrollArrow() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const ScrollArrow = () => {
+  const [showScroll, setShowScroll] = useState(false);
 
-export default ScrollArrow
+  useEffect(() => {
+    window.addEventListener("scroll", checkScrollTop);
+    return function cleanup() {
+      window.removeEventListener("scroll", checkScrollTop);
+    };
+  });
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 400) {
+      setShowScroll(true);
+    } else if (showScroll && window.pageYOffset <= 400) {
+      setShowScroll(false);
+    }
+  };
+
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    <ScrollTop
+      className="scrollTop"
+      onClick={scrollTop}
+      style={{ height: 40, display: showScroll ? "flex" : "none" }}
+    />
+  );
+};
+
+export default ScrollArrow;
