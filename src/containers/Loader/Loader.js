a8f9036d6f20a357497948e2b32043cca58b8d6e
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-29 22:40:15 +0100

    Loader component created

---
+++ src/containers/Loader/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { RotateCircleLoading } from "react-loadingg";
+
+function Loader() {
+  return (
+    <div>
+      <RotateCircleLoading color="#ffffff" />
+    </div>
+  );
+}
+
+export default Loader;
