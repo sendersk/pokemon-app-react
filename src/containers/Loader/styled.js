e28ac3c136ced607e31ad66cc92da4488e69d22b
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-30 23:29:06 +0100

    LoaderParagraph font-size changed

--- src/containers/Loader/styled.js
+++ src/containers/Loader/styled.js
@@ -1,17 +1,21 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const LoaderWrapper = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: rgba(0,0,0,0.3);
-`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.3);
+`;
 
 export const LoaderParagraph = styled.p`
-    color: #fff;
-    padding: 10px;
-    font-size: 70px;
-    line-height: 60vh;
-`
+  color: #fff;
+  padding: 10px;
+  font-size: 50px;
+  line-height: 60vh;
+
+  @media (max-width: 450px){
+      font-size: 30px;
+  }
+`;
