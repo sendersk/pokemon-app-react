c9cf25e86d12a16529dade34f8e91e3b44141e43
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-31 11:30:43 +0100

    ThemeProvider added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
 import { Provider } from "react-redux";
 import Store from "./Store";
+import {Theme} from "./Theme";
 import { BrowserRouter } from "react-router-dom";
-import {GlobalStyle} from "./GlobalStyle"
+import { GlobalStyle } from "./GlobalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={Store}>
-        <GlobalStyle />
-        <App />
+        <ThemeProvider theme={Theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
