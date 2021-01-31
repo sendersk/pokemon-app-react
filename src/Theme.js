99832149ab49e7d30c5b0205e86813312598643f
Przemyslaw Senderski <psenderski94@gmail.com>
2021-01-31 11:51:50 +0100

    theme colors added

--- src/Theme.js
+++ src/Theme.js
@@ -3,4 +3,13 @@ export const Theme = {
         screenSmall: 450,
         screenMedium: 768,
     },
+
+    color: {
+        white: "#ffffff",
+        lightSkyBlue: "#87cefa",
+        royalBlue: "#4169e1",
+        black: "#000000",
+        stoneColdGrey: "#555555",
+        luckyGrey: "#777777",
+    }
 } 
