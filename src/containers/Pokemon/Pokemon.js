68224c06aa4bd583aea882fa2d6a1c7785e9c863
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-20 19:35:37 +0100

    PokemonItem component added

--- src/containers/Pokemon/Pokemon.js
+++ src/containers/Pokemon/Pokemon.js
@@ -5,7 +5,7 @@ import _ from "lodash";
 import { Wrapper, Item, PokemonWrapper, PokemonHeader } from "./styled";
 import Alert from "../Alert/Alert";
 import Loader from "../Loader/Loader";
-import PokemonItem from "../PokemonItem/PokemonItem"
+import PokemonItem from "../PokemonItem/PokemonItem";
 
 const Pokemon = (props) => {
   const pokemonName = props.match.params.pokemon;
@@ -23,22 +23,31 @@ const Pokemon = (props) => {
         <Wrapper>
           <Item>
             <h1>Sprites</h1>
-          <img src={pokeData.sprites.front_default} alt="" />
+            <img src={pokeData.sprites.front_default} alt="" />
             <img src={pokeData.sprites.back_default} alt="" />
           </Item>
-          <PokemonItem title="Stats" content={pokeData.stats.map(({ stat, base_stat }) => {
+          <PokemonItem
+            title="Stats"
+            content={pokeData.stats.map(({ stat, base_stat }) => {
               return (
                 <p key={stat.name}>
                   {stat.name} {base_stat}
                 </p>
               );
-            })} />
-          <PokemonItem title="Abilities" content={pokeData.abilities.map(({ ability }) => {
+            })}
+          />
+          <PokemonItem
+            title="Abilities"
+            content={pokeData.abilities.map(({ ability }) => {
               return <p key={ability.name}>{ability.name}</p>;
-            })} />
-          <PokemonItem title="Type" content={pokeData.types.map(({ type }) => {
+            })}
+          />
+          <PokemonItem
+            title="Type"
+            content={pokeData.types.map(({ type }) => {
               return <p key={type.name}>{type.name}</p>;
-            })} />
+            })}
+          />
         </Wrapper>
       );
     }
