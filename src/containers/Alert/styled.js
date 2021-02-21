f6f7c5946bedc1d33a61faa929593d6956cac860
Przemyslaw Senderski <psenderski94@gmail.com>
2021-02-21 16:41:19 +0100

    Wrapper styles modified

--- src/containers/Alert/styled.js
+++ src/containers/Alert/styled.js
@@ -2,17 +2,21 @@ import styled from "styled-components"
 
 export const AlertWrapper = styled.div`
     width: 100%;
-    height: 100%;
+    height: 60vh;
     display: flex;
     justify-content: center;
     align-items: center;
+    border: 3px solid ${({theme}) => theme.color.red};
+    border-radius: 15px;
+    background-color: ${({theme}) => theme.color.alto};
+    padding: 10px;
 `
 
 export const AlertParagraph = styled.p`
-    color: ${({theme}) => theme.color.white};
-    font-size: 40px;
+    color: ${({theme}) => theme.color.red};
+    font-size: 32px;
 
     @media (max-width: ${({theme}) => theme.breakpoint.screenSmall}){
-        font-size: 26px;
+        font-size: 18px;
     }
 `
