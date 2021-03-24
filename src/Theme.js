8dd639bb6d4be7f07c322e2231d18e0d05aa0610
Przemyslaw Senderski <psenderski94@gmail.com>
2021-03-24 22:02:21 +0100

    theme colors order changed

--- src/Theme.js
+++ src/Theme.js
@@ -1,17 +1,17 @@
 export const Theme = {
-    breakpoint: {
-        screenSmall: 450,
-        screenMedium: 768,
-    },
+  breakpoint: {
+    screenSmall: 450,
+    screenMedium: 768,
+  },
 
-    color: {
-        white: "#ffffff",
-        alto: "#dddddd",
-        lightSkyBlue: "#87cefa",
-        royalBlue: "#4169e1",
-        red: "#ff0324",
-        black: "#000000",
-        stoneColdGrey: "#555555",
-        luckyGrey: "#777777",
-    }
-} 
+  color: {
+    white: "#ffffff",
+    red: "#ff0324",
+    alto: "#dddddd",
+    lightSkyBlue: "#87cefa",
+    luckyGrey: "#777777",
+    stoneColdGrey: "#555555",
+    royalBlue: "#4169e1",
+    black: "#000000",
+  },
+};
